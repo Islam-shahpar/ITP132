f6b193e6e50c84d12803fcc47d54a659102e510e
islam-shahpar <si2382@email.vccs.edu>
2020-10-08 18:47:01 -0400

    Added a new file called L7_DB.cpp

---
+++ L7_DB.cpp
@@ -0,0 +1,10 @@
+//Declare 3 variables for name, age, gender
+//cout object to display the message
+//cin object to get the input
+//write 1st if condition to check the age
+    // age < 18 -> No
+    // age > 60 -> No
+    // 18 < age <= 60 -> yes
+    //write 2nd if condition to check the gender
+        // gender == Male -> G/P/B
+        // gender == female -> A/S/P
