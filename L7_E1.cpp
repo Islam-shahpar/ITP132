aef2c4ec99369c217c46f0499d43e83a5a86e15a
islam-shahpar <si2382@email.vccs.edu>
2020-10-13 20:49:14 -0400

    update

--- L7_E1.cpp
+++ L7_E1.cpp
@@ -10,15 +10,46 @@ Coder :              Shahpar Islam
 using namespace std;
 
 
-int main() {
-  int sum = 0;
+// int main() {
+//   int sum = 0;
   
-  for (int number = 1; number <= 100; ++number){
-    sum = sum + number;
-  }  
+//   for (int number = 1; number <= 100; ++number){
+//     sum = sum + number;
+//   }  
 
-  cout << "Total sum : " << sum << endl;
+//   cout << "Total sum : " << sum << endl;
 
+//   return 0;
+// }
+
+// int main(){
+//   int sum = 0;
+//   string anything = "";
+//   cout << "Please provide your country name: " << endl;
+//   cin >> anything;
+
+//   cout << "The length of anything is : " << anything.length() << endl;
+
+//   for(int i = anything.length()-1; i > 0; i/=2){
+//     cout << "Value of index " << i << " and value is: " << anything[i] << endl;
+//   }
+//   return 0;
+// }
+
+
+int main(){
+  int sum = 0;
+  string anything = "";
+  cout << "Please provide your country name: " << endl;
+  cin >> anything;
+
+  cout << "The length of anything is : " << anything.length() << endl;
+
+  int i = anything.length() - 1;
+  do{
+    cout << "Value of index " << i << " and value is: " << anything[i] << endl;
+    i/=2;
+  } while(i > 0);
   return 0;
 }
 
