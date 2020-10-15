47f9a141bf14be82b6de6c7d9a9ef1e364ec9e63
islam-shahpar <si2382@email.vccs.edu>
2020-10-15 10:41:59 -0400

    created student code file

---
+++ student_code/creat_file.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function create_file(){
+    for ((i = 1; i <= $2; i++)); do
+        cp ../codes/L_Skeleton.cpp ../codes/L$1\_E$i.cpp
+    done
+}
+
+echo "Enter the Lecture number: "
+read lecture_number
+
+echo "Enter the Exercise number: "
+read exercise_number
+
+create_file $lecture_number $exercise_number
+
+
+
---
+++ student_code/remove_file.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function remove_file() {
+    for ((i=1; i<= $2; i++)); do
+        rm -rf ../codes/L$1\_E$i.cpp
+    done    
+}
+
+echo "Enter the Lecture number: "
+read lecture_number
+
+echo "Enter the Exercise number: "
+read exercise_number
+
+remove_file $lecture_number $exercise_number
+
+
