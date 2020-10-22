d257f8fe0473a5a9be0de6e67104b440cf643447
islam-shahpar <si2382@email.vccs.edu>
2020-10-22 02:04:07 -0400

    added selection sort

--- Class_notes/L9_DB.cpp
+++ Class_notes/L9_DB.cpp
@@ -1,9 +1,10 @@
 /*
 * Problem 1 : That calculates and displays the length of a 
-* right triangle hypotenuse  (Use the <cmath> header, pow() and sqrt function)
-*
-*
-*
+*             right triangle hypotenuse  (Use the <cmath> header,
+*             pow() and sqrt function)
+* Problem 2 : Declare an array | Access | Change | Loop through array
+* Problem 3 : Create a simple function that takes an array as input 
+*             then sort the array (ascending order)
 */
 
 #include <iostream>
@@ -15,6 +16,7 @@ void hypotenuse(){
     cout << "Please provide the length of first leg : " << endl;
     int firstLeg = 0;
     cin >> firstLeg;
+    
     cout << "Please provide the length of second leg : " << endl;
     int secondLeg = 0;
     cin >> secondLeg;
@@ -43,8 +45,53 @@ void learnArray(){
 
 }
 
+// void arraySort(int toBeSortedArray[10]){
+//     int len = sizeof(toBeSortedArray)/sizeof(toBeSortedArray[0]);
+//     for(int i = 0; i < (len - 1); i++){
+//         for(int j =1; j < len ; j++){
+//             if(toBeSortedArray[i] < toBeSortedArray[j]){
+//                 continue;
+//             }else{
+//                 int temp = toBeSortedArray[i];
+//                 toBeSortedArray[i] = toBeSortedArray[j];
+//                 toBeSortedArray[j] = temp;
+//             }
+//         }
+//     }
+// }
+
 int main(){
     //hypotenuse();
-    learnArray();
+    //learnArray();
+    int arrayToBeSorted[10];
+    cout << "Please input random number in array : " << endl;
+    for(int i = 0; i < 10; i++){
+        cin >> arrayToBeSorted[i];
+    }
+    cout << "Unsorted Array looks like this : " << endl;
+    for(int i = 0; i < 10; i++){
+        cout << arrayToBeSorted[i] << " ";
+    }
+    cout << endl;
+    //arraySort(arrayToBeSorted);
+    int min_index = 0;
+    int len = sizeof(arrayToBeSorted)/sizeof(arrayToBeSorted[0]);
+    for(int i = 0; i < (len - 1); i++){
+        min_index = i;
+        for(int j = i+1; j < len ; j++){
+            if(arrayToBeSorted[j] < arrayToBeSorted[min_index]){
+                min_index = j;
+                int temp = arrayToBeSorted[min_index];
+                arrayToBeSorted[min_index] = arrayToBeSorted[i];
+                arrayToBeSorted[i] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted Array looks like this : " << endl;
+    for(int i = 0; i < 10; i++){
+        cout << arrayToBeSorted[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
