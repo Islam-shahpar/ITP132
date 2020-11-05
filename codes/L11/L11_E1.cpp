9d724453c61e8dfcce3ba19ccc35b936e703bf7f
islam-shahpar <si2382@email.vccs.edu>
2020-11-05 15:32:33 -0500

    eidt L11_E1

--- codes/L11/L11_E1.cpp
+++ codes/L11/L11_E1.cpp
@@ -12,7 +12,21 @@ using namespace std;
 
 int main(){
   
-  
+  //declare an array
+  int newArray[5] = {100,101,102,103,104};
+
+  //enter data
+  int anotherArray[5];
+  anotherArray[0] = 100;
+  anotherArray[1] = 101;
+  anotherArray[2] = 102;
+  anotherArray[3] = 103;
+  anotherArray[4] = 104;
+
+  //Display data
+  for (int i = 0; i < 5; i++){
+      cout << newArray[i] << endl;
+  }
   
   return 0;
 }
